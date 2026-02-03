95daa8a5c8bcfc59e6c79ec5433e035a3f0fd0c1
RangaswamyGL <58364814+RangaswamyGL@users.noreply.github.com>
2026-02-03 22:59:47 +0530

    Update App.java

--- src/main/java/com/example/App.java
+++ src/main/java/com/example/App.java
@@ -2,7 +2,7 @@ package com.example;
 
 public class App {
     public static void main(String[] args) {
-        System.out.println("Hello World from Jenkins + Maven + Git 🚀");
+        System.out.println("Hello Guys from Jenkins + Maven + Git 🚀");
     }
 }
 
